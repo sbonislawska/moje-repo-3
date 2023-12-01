233a176e91e58b423b4e06b16d698a589984dc6f
sbonislawska <146773383+sbonislawska@users.noreply.github.com>
2023-12-01 21:08:43 +0100

    Add files via upload

---
+++ sprint3zadanie.js
@@ -0,0 +1,107 @@
+let numer = 5;
+let name = "Sylwia";
+let fruit = "";
+fruit = fruit + "jabłko";
+console.log(fruit);
+const user = {
+  name: "Sylwia",
+  age: 400,
+};
+let dog = "owczarek niemiecki";
+const cat = "szkocki";
+let sum = 7 + 2;
+let FirstLastName = "Sylwia Bonisławska";
+console.log(FirstLastName);
+let value = 1.2;
+let xc;
+xc = 2 + 3;
+console.log(xc);
+let city = "Ostrołęka";
+const tab = [22, "rower", true];
+const car = {
+  brand: "Toyota",
+  model: "Yaris",
+  year: 2016,
+};
+const tab2 = [4, 5, 6];
+let person = {
+  name: "Jan",
+  age: 36,
+  isStudent: false,
+};
+let obj = {};
+obj = {
+  color: "czerwony",
+};
+console.log(obj);
+let animals = [
+  { name: "cat", age: 1 },
+  { name: "dog", age: 5 },
+  { name: "horse", age: 4 },
+];
+let tab3 = [true, false, false];
+let student = {
+  name: "",
+  grades: [],
+  isActive: false,
+};
+let data = [23, true, [{}], "string"];
+let book = {
+  title: "Potęga podświadomości",
+  author: "Joseph Murphy",
+  yearPublish: 2022,
+};
+let number = 15;
+if (number > 10) {
+  console.log("Liczba większa od 10");
+}
+let text = "Hello";
+if (text === "Hello") {
+  console.log("Zmienna jest równa Hello");
+}
+const temperature = 25;
+const summer = temperature > 20 ? true : false;
+console.log(summer);
+let age = 18;
+if (age >= 18) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+const x = [];
+if (x.length == 0) {
+  console.log("Tablica jest pusta");
+} else {
+  console.log("Tablica nie jest pusta");
+}
+let isMember = true;
+isMember == true ? "otrzymujesz rabat" : "nie otrzymujesz rabatu";
+const rabat = isMember == true ? "otrzymujesz rabat" : "nie otrzymujesz rabatu";
+console.log(rabat);
+const characters = "pracadomowa";
+if (characters.lenght > 5) {
+  console.log("Znaków jest więcej niż 5");
+}
+const exercise = 2;
+if (exercise / 2) {
+  console.log("liczba parzysta");
+} else {
+  console.log("liczba nieparzysta");
+}
+let sAge = 40;
+const myAge = sAge >= 40 ? " jestem stara" : "nie jestem stara";
+console.log(myAge);
+const obj2 = {
+  name: "Tower",
+  age: 12,
+};
+const object = {
+  name: "Anna",
+};
+if ("name" in object) {
+  console.log("W objekcie znajduje się klucz name");
+} else {
+  console.log("W obiekcie nie ma klucza name");
+}
+{
+}
